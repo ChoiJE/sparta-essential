d85e33685b1c0eeeb6066a7e343461d2eecaa4e0
ChoiJE <wjddms8031@naver.com>
2021-09-25 21:37:35 +0900

    Add Question detail page & Add href link to list page & Exclude published_after now

--- 05_week/spart/polls/views.py
+++ 05_week/spart/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 from django.utils import timezone
 from django.http import HttpResponse
@@ -11,3 +11,7 @@ def index(request):
     context = {'question_list': question_list}
 
     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id, pub_date__lte=timezone.now())
+    return render(request, 'polls/detail.html', {'question': question})
